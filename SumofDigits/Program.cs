411986ae70f6f324a3044cb85af3f2e2a68d9f09
ShamilHajiyev <shamilnh@code.edu.az>
2022-10-19 17:55:36 +0400

    initial commit

---
+++ SumofDigits/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SumofDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task: Verilmiş tam müsbət n ədədinin rəqəmlərinin cəmini tapan alqoritm
+
+            Console.WriteLine("Enter a number:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            while (n != 0)
+            {
+                sum += n % 10;
+                n /= 10;
+            }
+            Console.WriteLine("\nSum of digits:\n" + sum);
+        }
+    }
+}
